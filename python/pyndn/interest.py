428936f6b16d48fc88404d3291d510055ae7ee8b
Jeff Thompson <jefft0@gmail.com>
2014-02-07 15:16:58 -0800

    Interest: Implement getChangeCount and use in getNonce.

--- python/pyndn/interest.py
+++ python/pyndn/interest.py
@@ -5,6 +5,8 @@
 #
 
 from encoding import WireFormat
+from pyndn.util import Blob
+from pyndn.util import ChangeCounter
 from name import Name
 
 """
@@ -13,12 +15,40 @@ This module defines the NDN Interest class.
 
 class Interest(object):
     def __init__(self, name = None):
-        if name == None:
-            name = Name()
-        self._name = name
+        self._name = ChangeCounter(name if type(name) == Name else Name(name))
+        self._nonce = Blob()
+        
+        self._getNonceChangeCount = 0
+        self._changeCount = 0
 
     def getName(self):
-        return self._name
+        return self._name.get()
+    
+    def getNonce(self):
+        """
+        Return the nonce value from the incoming interest.  If you change any of
+        the fields in this Interest object, then the nonce value is cleared.
+        
+        :return: The nonce.  If isNull() then there nonce is omitted.
+        :rtype: Blob
+        """
+        if self._getNonceChangeCount != self.getChangeCount():
+            # The values have changed, so the existing nonce is invalidated.
+            self._nonce = Blob()
+            self._getNonceChangeCount = self.getChangeCount()
+
+        return self._nonce
+    
+    def setName(self, name):
+        self._name = ChangeCounter(name if type(name) == Name else Name(name))
+        self._changeCount += 1
+    
+    def setNonce(self, nonce):
+        self._nonce = nonce if type(nonce) == Blob else Blob(nonce)
+        # Set _getNonceChangeCount so that the next call to getNonce() won't 
+        #   clear _nonce.
+        self._changeCount += 1
+        self._getNonceChangeCount = self.getChangeCount();
     
     def wireEncode(self, wireFormat = WireFormat.getDefaultWireFormat()):
         """
@@ -41,4 +71,20 @@ class Interest(object):
         :type wireFormat: A subclass of WireFormat.
         """
         wireFormat.decodeInterest(self, input)
+        
+    def getChangeCount(self):
+        """
+        Get the change count, which is incremented each time this object 
+        (or a child object) is changed.
+        
+        :return: The change count.
+        :rtype: int
+        """
+        # Make sure each of the checkChanged is called.
+        changed = self._name.checkChanged()
+        if changed:
+            # A child object has changed, so update the change count.
+            self._changeCount += 1
+
+        return self._changeCount
         
