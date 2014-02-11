1400c60382bf5c6bba418fb893715655623b8c36
Jeff Thompson <jefft0@gmail.com>
2014-02-10 17:24:00 -0800

    Name: Added URI decoding.

--- python/pyndn/name.py
+++ python/pyndn/name.py
@@ -5,16 +5,45 @@
 # See COPYING for copyright and distribution information.
 #
 
-from io import BytesIO
-from pyndn.util import Blob
-
 """
 This module defines the NDN Name class.
 """
 
+from io import BytesIO
+from pyndn.util import Blob
+
 class Name(object):
+    """
+    Create a new Name which holds an array of Name.Component and represents an 
+    NDN name.
+    
+    :param value: (optional) If value is another Name, then copy it.  If value 
+      is a str then call set(value) to create from the URI.  If ommitted,
+      create an empty name.
+    :type value: Name or str
+    """
+    def __init__(self, value = None):
+        if type(value) is Name:
+            # Copy the components array, but don't need to copy each Component.
+            self._components = value._components[:]
+        elif type(value) is str:
+            self._components = []
+            self.set(value)
+        else:
+            self._components = []
+            
+        self._changeCount = 0
+        
     class Component(object):
-        def __init__(self, value):
+        """
+        Create a new Name.Component.
+        
+        :param value: (optional) If value is already a Blob or Name.Component,
+          then take another pointer to the value.  Otherwise, create a new
+          Blob with a copy of the value.  If omitted, create an empty component.
+        :type value: Blob or Name.Component or value for Blob constructor
+        """
+        def __init__(self, value = None):
             if type(value) == Name.Component:
                 # Use the existing Blob in the other Component.
                 self._value = value._value
@@ -23,13 +52,90 @@ class Name(object):
                 self._value = value if isinstance(value, Blob) else Blob(value)
             
         def getValue(self):
-            return self._value
+            """
+            Get the value of the component.
 
-    def __init__(self, value = None):
-        self._components = []
-        self._changeCount = 0
+            :return: The component value.
+            :rtype: Blob
+            """
+            return self._value
         
+        def toEscapedString(self, result = None):
+            """
+            Convert this component to a string, escaping characters according
+            to the NDN URI Scheme. This also adds "..." to a value with zero or
+            more ".".
+
+            :param result: (optional) The BytesIO stream to write to.  If 
+              omitted, return a str with the result.
+            :type result: BytesIO
+            :return: The result as a string (only if result is omitted).
+            :rtype: str
+            """
+            if result == None:
+                return Name.toEscapedString(self._value.buf())
+            else:
+                Name.toEscapedString(self._value.buf(), result)
+
+    def set(self, uri):
+        """
+        Parse the uri according to the NDN URI Scheme and set the name with 
+        the components.
+        
+        :param uri: The URI string.
+        :type uri: str
+        """
+        self.clear()
+  
+        uri = uri.strip()
+        if len(uri) == 0:
+            return
+
+        iColon = uri.find(':')
+        if iColon >= 0:
+            # Make sure the colon came before a '/'.
+            iFirstSlash = uri.find('/')
+            if iFirstSlash < 0 or iColon < iFirstSlash:
+                # Omit the leading protocol such as ndn:
+                uri = uri[iColon + 1:].strip()
+
+        # Trim the leading slash and possibly the authority.
+        if uri[0] == '/':
+            if len(uri) >= 2 and uri[1] == '/':
+                # Strip the authority following "//".
+                iAfterAuthority = uri.find('/', 2)
+                if iAfterAuthority < 0:
+                    # Unusual case: there was only an authority.
+                    return
+                else:
+                    uri = uri[iAfterAuthority + 1:].strip()
+            else:
+                uri = uri[1:].strip()
+
+        iComponentStart = 0
+  
+        # Unescape the components.
+        while iComponentStart < len(uri):
+            iComponentEnd = uri.find('/', iComponentStart)
+            if iComponentEnd < 0:
+                iComponentEnd = len(uri)
+    
+            component = Name.fromEscapedString(uri, iComponentStart, 
+                                               iComponentEnd)
+            # Ignore illegal components.  This also gets rid of a trailing '/'.
+            if not component.isNull():
+                self.append(component)
+    
+            iComponentStart = iComponentEnd + 1
+
     def append(self, value):
+        """
+        Append a new component.
+        
+        :param value: If value is another Name.Component, use its value.
+          Otherwise pass value to the Name.Component constructor.
+        :type value: Name.Component or value for Name.Component constructor
+        """
         if isinstance(value, Name.Component):
             self._components.append(value)
         else:
@@ -38,10 +144,29 @@ class Name(object):
         self._changeCount += 1
         return self
 
+    def size(self):
+        """
+        Get the number of components.
+        
+        :return: The number of components.
+        :rtype: int
+        """
+        return len(self._components)
+
     def get(self, i):
+        """
+        Get the component at the given index.
+        
+        :param i: The index of the component, starting from 0.  However, if i is
+          negative, return the component at size() - (-i).
+        :type i: int
+        """
         return self._components[i]
     
     def clear(self):
+        """
+        Clear all the components.
+        """
         self._components = []
         self._changeCount += 1
     
@@ -60,7 +185,7 @@ class Name(object):
         for component in self._components:
             # write is required to take a byte buffer.
             result.write(Name._slash)
-            self.toEscapedString(component._value.buf(), result)
+            component.toEscapedString(result)
   
         value = result.getvalue()
         if not type(value) is str:
@@ -76,31 +201,80 @@ class Name(object):
         :return: The change count.
         :rtype: int
         """
+        return self._changeCount
         
-    _dot = bytearray([ord('.')])
     @staticmethod
-    def toEscapedString(value, result):
+    def fromEscapedString(escapedString, beginOffset = 0, endOffset = None):
         """
-        Write the value to result, escaping characters according to the NDN URI 
-        Scheme. This also adds "..." to a value with zero or more ".".
+        Make a Blob value by decoding the escapedString between beginOffset and
+        endOffset according to the NDN URI Scheme.  (If offsets are omitted,
+        then decode the whole string.)  If the escaped string is "", "." or ".."
+        then return a Blob with a null pointer, which means the component should
+        be skipped in a URI name.
         
-        :param value: The buffer with the value to escape.
-        :type value: An array type with int elements.
-        :param result: The BytesIO stream to write to.
-        :type result: BytesIO
+        :param escapedString: The escaped string.
+        :type escapedString: str
+        :return: The unescaped Blob value. If the escapedString is not a valid 
+          escaped component, then the Blob isNull().
         """
+        if endOffset == None:
+            endOffset = len(escapedString)
+        value = Name._unescape(escapedString[beginOffset:endOffset].strip())
+        
         gotNonDot = False
         for i in range(len(value)):
-            if value[i] != Name._dot[0]:
+            if value[i] != '.':
                 gotNonDot = True
                 break
 
         if not gotNonDot:
+            # Special case for component of only periods.
+            if len(value) <= 2:
+                # Zero, one or two periods is illegal.  Ignore this component.
+                return Blob()
+            else:
+                # Remove 3 periods.
+                return Blob(value[3:])
+        else:
+            return Blob(value)
+        
+    @staticmethod
+    def toEscapedString(value, result = None):
+        """
+        Convert value to a string, escaping characters according to the NDN URI 
+        Scheme. This also adds "..." to a value with zero or more ".".
+        
+        :param value: The buffer with the value to escape.
+        :type value: An array type with int elements.
+        :param result: (optional) The BytesIO stream to write to.  If omitted,
+          return a str with the result.
+        :type result: BytesIO
+        :return: The result as a string (only if result is omitted).
+        :rtype: str
+        """
+        if result == None:
+            result = BytesIO()
+            Name.toEscapedString(value, result)
+            
+            value = result.getvalue()
+            if not type(value) is str:
+                # Assume value is a Python 3 bytes object.  Convert to string.
+                value = str(value, encoding = "Latin-1")
+            return value            
+            
+        gotNonDot = False
+        for i in range(len(value)):
+            if value[i] != ord('.'):
+                gotNonDot = True
+                break
+
+        charBuffer = bytearray(1)
+        if not gotNonDot:
+            charBuffer[0] = ord('.')
             # Special case for component of zero or more periods. Add 3 periods.
             for i in range(len(value) + 3):
-                result.write(Name._dot)
+                result.write(charBuffer)
         else:
-            charBuffer = bytearray(1)
             hexBuffer = bytearray(3)
             hexBuffer[0] = ord('%')
             for i in range(len(value)):
@@ -130,3 +304,42 @@ class Name(object):
             return self._components[key]
         else:
             raise ValueError("Unknown __getitem__ type: %s" % type(key))
+
+    @staticmethod
+    def _unescape(escaped):
+        """
+        A pritave method to return a copy of the escaped string, converting 
+        each escaped "%XX" to the char value.
+        
+        :param escaped: The escaped string.
+        :type escaped: str
+        :return: The unescaped buffer
+        :rtype: bytearray
+        """
+        result = BytesIO()
+
+        buffer = bytearray(1)
+        i = 0
+        while i < len(escaped):
+            if escaped[i] == '%' and i + 2 < len(escaped):
+                try:
+                    buffer[0] = int(escaped[i + 1:i + 3], 16)
+                    result.write(buffer)
+                except ValueError:
+                    # Invalid hex characters.  An unusual case, so just keep 
+                    #   the escaped string.
+                    for j in range(i, i + 3):
+                        buffer[0] = ord(escaped[j])
+                        result.write(buffer)
+                    
+                # Skip ahead past the escaped value.
+                i += 2
+            else:
+                # Just copy through.
+                buffer[0] = ord(escaped[i])
+                # write is required to take a byte buffer.
+                result.write(buffer)
+
+            i += 1
+
+        return bytearray(result.getvalue())          
