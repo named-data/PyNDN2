95fb5346ea1b450e61f0752b812a5f8f3b8a048d
Jeff Thompson <jefft0@gmail.com>
2014-02-21 13:05:41 -0800

    tests: In test_get_async, revert to connecting to spurs.cs.ucla.edu.

--- tests/test_get_async.py
+++ tests/test_get_async.py
@@ -30,11 +30,11 @@ class Counter(object):
         dump("Time out for interest", interest.getName().toUri()) 
 
 def main():
-    face = Face("localhost")
+    face = Face("spurs.cs.ucla.edu")
     
     counter = Counter()
 
-    name1 = Name("/testecho/hello11");    
+    name1 = Name("/ndn/ucla.edu"); 
     dump("Express name ", name1.toUri())
     face.expressInterest(name1, counter.onData, counter.onTimeout)
 
