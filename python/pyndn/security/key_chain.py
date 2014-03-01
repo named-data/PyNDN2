3be9acad662826461880c9dd986204f601364b29
Jeff Thompson <jefft0@gmail.com>
2014-02-28 17:09:33 -0800

    security: In KeyChain, implement verifyData.

--- python/pyndn/security/key_chain.py
+++ python/pyndn/security/key_chain.py
@@ -43,6 +43,71 @@ class KeyChain(object):
         self._identityManager.signByCertificate(
           data, certificateName, wireFormat)
           
+          
+    def verifyData(self, data, onVerified, onVerifyFailed, stepCount = 0):
+        """
+        Check the signature on the Data object and call either onVerify or 
+        onVerifyFailed. We use callback functions because verify may fetch 
+        information to check the signature.
+        
+        :param data: The Data object with the signature to check. It is an error
+          if data does not have a wireEncoding. To set the wireEncoding, you can 
+          call data.wireDecode.
+        :type data: Data
+        :param onVerified: If the signature is verified, this calls 
+          onVerified(data).
+        :type onVerified: function object
+        :param onVerifyFailed: If the signature check fails or can't find the 
+          public key, this calls onVerifyFailed(data).
+        :type onVerifyFailed: function object
+        :param stepCount: (optional) The number of verification steps that have 
+          been done. If omitted, use 0.
+        :type stepCount: int
+        :return: 
+        :rtype: boolean
+        """
+        if self._policyManager.requireVerify(data):
+            nextStep = self._policyManager.checkVerificationPolicy(
+              data, stepCount, onVerified, onVerifyFailed)
+            if nextStep != None:
+                self._face.expressInterest(
+                  nextStep.interest, self._makeOnCertificateData(nextStep),
+                  self._makeOnCertificateInterestTimeout(
+                    nextStep.retry, onVerifyFailed, data, nextStep))
+        elif self._policyManager.skipVerifyAndTrust(data):
+            onVerified(data)
+        else:
+            onVerifyFailed(data)
+            
+    def _makeOnCertificateData(self, nextStep):
+        """
+        Make and return an onData callback to use in expressInterest.
+        """
+        def onData(interest, data):
+            # Try to verify the certificate (data) according to the parameters 
+            #   in nextStep.
+            self.verifyData(data, nextStep.onVerified, nextStep.onVerifyFailed, 
+                            nextStep.stepCount)
+        return onData
+
+    def _makeOnCertificateInterestTimeout(self, retry, onVerifyFailed, data, 
+                                          nextStep):
+        """
+        Make and return an onTimeout callback to use in expressInterest.
+        """
+        def onTimeout(interest):
+            if retry > 0:
+                # Issue the same expressInterest as in verifyData except 
+                #   decrement retry.
+                self._face.expressInterest(
+                  interest, self._makeOnCertificateData(nextStep), 
+                     self._makeOnCertificateInterestTimeout(
+                       retry, onVerifyFailed, data, nextStep))
+            else:
+                onVerifyFailed(data);
+        return onTimeout
+            
+            
     def setFace(self, face):
         """
         Set the Face which will be used to fetch required certificates.
