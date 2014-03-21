d76fd3f5036146d0accb7593e8d49b3b10d21ac9
Jeff Thompson <jefft0@gmail.com>
2014-03-21 14:09:16 -0700

    KeyChain: Add note comment for experimental feature.

--- python/pyndn/security/key_chain.py
+++ python/pyndn/security/key_chain.py
@@ -9,6 +9,8 @@
 This module defines the KeyChain class which provides a set of interfaces to the
 security library such as identity management, policy configuration  and packet 
 signing and verification.
+Note: This class is an experimental feature. See the API docs for more detail at
+  http://named-data.net/doc/ndn-ccl-api/key-chain.html .
 """
 
 class KeyChain(object):
