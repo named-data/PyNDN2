7ddbf6fcbc34028ed49fb4cabdaaf6d8c2981f3d
Jeff Thompson <jefft0@gmail.com>
2014-04-30 14:08:54 -0700

    Added contrib/cocoapy (for OS X support).

---
+++ python/pyndn/contrib/__init__.py
@@ -0,0 +1,5 @@
+# -*- Mode:python; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+#
+# Copyright (C) 2014 Regents of the University of California.
+# Author: Jeff Thompson <jefft0@remap.ucla.edu>
+# See COPYING for copyright and distribution information.
