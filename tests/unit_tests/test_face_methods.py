66876cf6410c816804aa38f190d5577694cdf44e
Adeola Bannis <thecodemaiden@gmail.com>
2014-06-12 16:24:15 -0700

    Added face registration test with gevent for threading

--- tests/unit_tests/test_face_methods.py
+++ tests/unit_tests/test_face_methods.py
@@ -1,9 +1,15 @@
 from pyndn import Name
 from pyndn import Face
+from pyndn import Data
+from pyndn import Blob
+
+from pyndn.security import KeyChain
 
 import unittest as ut
 import gevent
 import time
+
+from test_utils import CredentialStorage
 
 # use Python 3's mock library if it's available
 # else you'll have to pip install mock
@@ -26,30 +32,29 @@ class TestFaceInterestMethods(ut.TestCase):
         name = Name(interestName)
         dataCallback = Mock()
         timeoutCallback = Mock()
-        startTime = time.clock()
         self.face.expressInterest(name, dataCallback, timeoutCallback)
         
-        currentTime = startTime
-        while currentTime < startTime + timeout:
-            self.face.processEvents()
-            time.sleep(0.01)
-            currentTime = time.clock()
-
-            if (dataCallback.call_count > 0 or timeoutCallback.call_count > 0):
-                break
+        def waitForCallbacks():
+            while 1:
+                self.face.processEvents()
+                gevent.sleep(0.01)
+                if (dataCallback.call_count > 0 or timeoutCallback.call_count > 0):
+                    break
         
-        didTimeout = currentTime - startTime >= timeout
-        return dataCallback, timeoutCallback, didTimeout
+        task = gevent.spawn(waitForCallbacks)
+        task.join(timeout=10)
+
+        return dataCallback, timeoutCallback
 
 
 
     def test_any_interest(self):
         uri = "/"
-        (dataCallback, timeoutCallback, didTimeout) = self.run_express_name_test(uri)
-        self.assertTrue(timeoutCallback.call_count == 0 and not didTimeout , 'Timeout on expressed interest')
+        (dataCallback, timeoutCallback) = self.run_express_name_test(uri)
+        self.assertTrue(timeoutCallback.call_count == 0 , 'Timeout on expressed interest')
         
         # check that the callback was correct
-        self.assertEqual(dataCallback.call_count, 1, 'Data callback called more than once')
+        self.assertEqual(dataCallback.call_count, 1, 'Expected 1 onData callback, got '+str(dataCallback.call_count))
 
         onDataArgs = dataCallback.call_args[0] # the args are returned as ([ordered arguments], [keyword arguments])
 
@@ -59,11 +64,11 @@ class TestFaceInterestMethods(ut.TestCase):
         
     def test_specific_interest(self):
         uri = "/ndn/edu/ucla/remap/ndn-js-test/howdy.txt/%FD%052%A1%DF%5E%A4"
-        (dataCallback, timeoutCallback, didTimeout) = self.run_express_name_test(uri)
-        self.assertTrue(timeoutCallback.call_count == 0 and not didTimeout , 'Unexpected timeout on expressed interest')
+        (dataCallback, timeoutCallback) = self.run_express_name_test(uri)
+        self.assertTrue(timeoutCallback.call_count == 0, 'Unexpected timeout on expressed interest')
         
         # check that the callback was correct
-        self.assertEqual(dataCallback.call_count, 1, 'Data callback called more than once')
+        self.assertEqual(dataCallback.call_count, 1, 'Expected 1 onData callback, got '+str(dataCallback.call_count))
 
         onDataArgs = dataCallback.call_args[0] # the args are returned as ([ordered arguments], [keyword arguments])
 
@@ -73,12 +78,10 @@ class TestFaceInterestMethods(ut.TestCase):
 
     def test_timeout(self):
         uri = "/test/timeout"
-        (dataCallback, timeoutCallback, didTimeout) = self.run_express_name_test(uri)
+        (dataCallback, timeoutCallback) = self.run_express_name_test(uri)
 
         # we're expecting a timeout callback, and only 1
         self.assertTrue(dataCallback.call_count == 0, 'Data callback called for invalid interest')
-
-        self.assertFalse(didTimeout, 'Timeout forced before timeout callback called')
 
         self.assertTrue(timeoutCallback.call_count == 1, 'Expected 1 timeout call, got ' + str(timeoutCallback.call_count))
 
@@ -95,24 +98,97 @@ class TestFaceInterestMethods(ut.TestCase):
         timeoutCallback = Mock()
 
         interestID = self.face.expressInterest(name, dataCallback, timeoutCallback)
-        startTime = time.clock()
-        currentTime = startTime
-        timeout = 0.2
-        while currentTime < startTime + timeout:
-            self.face.processEvents()
-            time.sleep(0.01)
-            # hopefully a response takes more that 0.01s to come back...
+
+        def removeInterestAndWait():
             self.face.removePendingInterest(interestID)
-            currentTime = time.clock()
-            if (dataCallback.call_count > 0 or timeoutCallback.call_count > 0):
-                break
-        
-        didTimeout = currentTime - startTime >= timeout
+            while 1:
+                self.face.processEvents()
+                gevent.sleep(0.01)
+                currentTime = time.clock()
+                if (dataCallback.call_count > 0 or timeoutCallback.call_count > 0):
+                    break
+
+        task = gevent.spawn(removeInterestAndWait)
+        task.join(timeout=10)
 
         self.assertEqual(dataCallback.call_count, 0, 'Should not have called data callback after interest was removed')
         self.assertEqual(timeoutCallback.call_count, 0, 'Should not have called timeout callback after interest was removed')
         
+class TestFaceRegisterMethods(ut.TestCase):
+    def setUp(self):
+        self.face_in = Face()
+        self.face_out = Face()
+        self.keyChain = KeyChain()
+
+    def tearDown(self):
+        self.face_in.shutdown()
+        self.face_out.shutdown()
+
+    def onInterestEffect(self, prefix, interest, transport, prefixID):
+        data = Data(interest.getName())
+        data.setContent("SUCCESS")
+        self.keyChain.sign(data, self.keyChain.getDefaultCertificateName())
+        encodedData = data.wireEncode()
+        transport.send(encodedData.toBuffer())
+
+    
+    def test_register_prefix_response(self):
+        # gotta sign it (WAT)
+        prefixName = Name("/unittest")
+        self.face_in.setCommandSigningInfo(self.keyChain, 
+                self.keyChain.getDefaultCertificateName())
+
+        failedCallback = Mock()
+        interestCallback = Mock(side_effect=self.onInterestEffect)
+
+        self.face_in.registerPrefix(prefixName, interestCallback, failedCallback)
+        server = gevent.spawn(self.face_process_events, self.face_in, [interestCallback, failedCallback], 'h')
+        
+        gevent.sleep(1) # give the 'server' time to register the interest
+
+        # express an interest on another face
+        dataCallback = Mock()
+        timeoutCallback = Mock()
+
+        # now express an interest on this new face, and see if onInterest is called
+        interestName = prefixName.append("hello")
+        self.face_out.expressInterest(interestName, dataCallback, timeoutCallback)
+
+        client = gevent.spawn(self.face_process_events, self.face_out, [dataCallback, timeoutCallback], 'c')
+        
+        gevent.joinall([server, client], timeout=10) 
+
+        self.assertEqual(failedCallback.call_count, 0, 'Failed to register prefix at all') 
+
+        self.assertEqual(interestCallback.call_count, 1, 'Expected 1 onInterest callback, got '+str(interestCallback.call_count))
+
+        self.assertEqual(dataCallback.call_count, 1, 'Expected 1 onData callback, got '+str(dataCallback.call_count))
+
+        onDataArgs = dataCallback.call_args[0]
+        # check the message content
+        data = onDataArgs[1]
+        expectedBlob = Blob(bytearray("SUCCESS"))
+        self.assertTrue(expectedBlob.equals(data.getContent()), 'Data received on face does not match expected format')
+
+
+        
+
+    def face_process_events(self, face, callbacks, name=None):
+        # implemented as a 'greenlet': something like a thread, but semi-synchronous
+        # callbacks should be a list
+        done = False
+        while not done:
+            face.processEvents()
+            gevent.sleep(0.01)
+            for c in callbacks:
+                
+                if (c.call_count > 0):
+                    done = True
+
+
 
 if __name__ == '__main__':
     suite = ut.TestLoader().loadTestsFromTestCase(TestFaceInterestMethods)
+    ut.TextTestRunner(verbosity=2).run(suite)
+    suite = ut.TestLoader().loadTestsFromTestCase(TestFaceRegisterMethods)
     ut.TextTestRunner(verbosity=2).run(suite)
