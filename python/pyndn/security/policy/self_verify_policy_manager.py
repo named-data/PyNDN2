073ac74e2fef4737dd5b5347fb1db505e1590b4f
Jeff Thompson <jefft0@remap.ucla.edu>
2014-07-28 00:25:26 -0700

    security (minor): In SelfVerifyPolicyManager.checkVerificationPolicy fix a typo in the doc comment.

--- python/pyndn/security/policy/self_verify_policy_manager.py
+++ python/pyndn/security/policy/self_verify_policy_manager.py
@@ -81,8 +81,8 @@ class SelfVerifyPolicyManager(PolicyManager):
                                 onVerifyFailed, wireFormat = None):
         """
         Look in the IdentityStorage for the public key with the name in the 
-        KeyLocator (if available) and use it to verify the data packet. If the 
-        public key can't be found, call onVerifyFailed.
+        KeyLocator (if available) and use it to verify the data packet or
+        signed interest. If the public key can't be found, call onVerifyFailed.
 
         :param dataOrInterest: The Data object or interest with the signature to
           check.
