5c0c94a6a6d4186e52f7e1405d357a572db3d26a
Jeff Thompson <jefft0@remap.ucla.edu>
2014-10-24 14:09:25 -0700

    security: Fix typo in ConfigPolicyManager._interestTimestampIsFresh call to toUri().

--- python/pyndn/security/policy/config_policy_manager.py
+++ python/pyndn/security/policy/config_policy_manager.py
@@ -63,7 +63,7 @@ class ConfigPolicyManager(SelfVerifyPolicyManager):
     
     :param IdentityStorage identityStorage: The IdentityStorage for 
       looking up the public key. This object must remain valid during the life 
-      of this SelfVerifyPolicyManager. 
+      of this ConfigPolicyManager.
     :param string configFileName: The path to the configuration file containing
       verification rules.
     :param int searchDepth: (optional) The maximum number of links to follow 
@@ -364,7 +364,7 @@ class ConfigPolicyManager(SelfVerifyPolicyManager):
         :paramt int timestamp: The timestamp extracted from the interest name.
         """
         try:
-            lastTimestamp = self._keyTimestamps[keyName.toUri]
+            lastTimestamp = self._keyTimestamps[keyName.toUri()]
         except KeyError:
             now = time.time()
             notBefore = now - self._keyGraceInterval
