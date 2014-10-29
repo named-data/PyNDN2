c77cca308291d7fb3f2c5e9df7bb8d0771d197cc
Jeff Thompson <jefft0@remap.ucla.edu>
2014-10-29 15:35:59 -0700

    Global: In copyright headers, always say "Lesser" General Public License. Refs #2028.

--- python/pyndn/security/policy/config_policy_manager.py
+++ python/pyndn/security/policy/config_policy_manager.py
@@ -1,4 +1,3 @@
-
 # -*- Mode:python; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
 #
 # Copyright (C) 2014 Regents of the University of California.
@@ -12,11 +11,11 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# A copy of the GNU General Public License is in the file COPYING.
+# A copy of the GNU Lesser General Public License is in the file COPYING.
 
 import sys
 import os
