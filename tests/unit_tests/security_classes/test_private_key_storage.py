89d8aabc9a18ef21efd9efa90e020433fb452497
Jeff Thompson <jefft0@remap.ucla.edu>
2014-12-03 12:27:19 -0800

    unit_tests: In TestPrivateKeyStorage, remove methods that are already implementing in the base class FilePrivateKeyStorage.

--- tests/unit_tests/security_classes/test_private_key_storage.py
+++ tests/unit_tests/security_classes/test_private_key_storage.py
@@ -36,52 +36,6 @@ from pyndn.security.security_exception import SecurityException
 from pyndn.security.identity.file_private_key_storage import FilePrivateKeyStorage
 
 class TestPrivateKeyStorage(FilePrivateKeyStorage):
-
-    def sign(self, data, keyName, digestAlgorithm = DigestAlgorithm.SHA256):
-        """
-        Fetch the private key for keyName and sign the data, returning a
-        signature Blob.
-
-        :param data: Pointer the input byte buffer to sign.
-        :type data: An array type with int elements
-        :param Name keyName: The name of the signing key.
-        :param digestAlgorithm: (optional) the digest algorithm. If omitted,
-          use DigestAlgorithm.SHA256.
-        :type digestAlgorithm: int from DigestAlgorithm
-        :return: The signature, or an isNull() Blob pointer if signing fails.
-        :rtype: Blob
-        """
-        if digestAlgorithm != DigestAlgorithm.SHA256:
-            raise SecurityException(
-              "FilePrivateKeyStorage.sign: Unsupported digest algorithm")
-
-        der = self.getPrivateKey(keyName)
-        privateKey = RSA.importKey(der.toRawStr())
-
-        # Sign the hash of the data.
-        if sys.version_info[0] == 2:
-            # In Python 2.x, we need a str.  Use Blob to convert data.
-            data = Blob(data, False).toRawStr()
-        signature = PKCS1_v1_5.new(privateKey).sign(SHA256.new(data))
-        # Convert the string to a Blob.
-        return Blob(bytearray(signature), False)
-
-    def doesKeyExist(self, keyName, keyClass):
-        """
-        Check if a particular key exists.
-
-        :param Name keyName: The name of the key.
-        :param keyClass: The class of the key, e.g. KeyClass.PUBLIC,
-           KeyClass.PRIVATE, or KeyClass.SYMMETRIC.
-        :type keyClass: int from KeyClass
-        :return: True if the key exists, otherwise false.
-        :rtype: bool
-        """
-        try:
-            return os.path.isfile(self._getTransformedName(keyName, keyClass))
-        except (AttributeError, SecurityException):
-            return False
-
     def _getTransformedName(self, keyName, keyClass):
         """
         Apply the name transformation and append the correct extension
@@ -103,30 +57,6 @@ class TestPrivateKeyStorage(FilePrivateKeyStorage):
         except NameError:
             # an invalid keyClass was given
             raise SecurityException('Invalid key type given')
-
-    def _getNewRsaKeyBits(self, keySize):
-        # returns public and private key DER in blobs
-        key = RSA.generate(keySize)
-        publicDer = key.publickey().exportKey(format='DER')
-        privateDer = key.exportKey(format='DER', pkcs=8)
-        return (Blob(publicDer, False), Blob(privateDer, False))
-
-    def generateKeyPair(self, keyName, keyType=KeyType.RSA, keySize=2048):
-        """
-        Generate a pair of asymmetric keys.
-        Your derived class should override.
-
-        :param Name keyName: The name of the key pair.
-        :param keyType: (optional) The type of the key pair.  If omitted, use
-          KeyType.RSA
-        :type keyType: int from KeyType
-        :param int keySize: (optional) The size of the key pair.  If omitted,
-          use 2048.
-        """
-        # TODO: different generator for DSA
-        public, private = self._getNewRsaKeyBits(keySize)
-        self.addPublicKey(keyName, public)
-        self.addPrivateKey(keyName, private)
 
     def deleteKeyPair(self, keyName):
         try:
@@ -153,37 +83,6 @@ class TestPrivateKeyStorage(FilePrivateKeyStorage):
         :param Blob keyDer: The private key DER
         """
         self._saveKey(keyName, keyDer, KeyClass.PRIVATE)
-
-    def getPrivateKey(self, keyName):
-        """
-        Fetch a private key from the store.
-        :param Name keyName: The name of the private key to look up.
-        :return: The binary DER encoding of the private key bits
-        :rtype: Blob
-        """
-        return self._loadKey(keyName, KeyClass.PRIVATE)
-
-    def getPublicKey(self, keyName):
-        """
-        Fetch a public key from the store.
-        :param Name keyName: The name of the public key to look up.
-        :return: The binary DER encoding of the public key bits
-        :rtype: Blob
-        """
-        return self._loadKey(keyName, KeyClass.PUBLIC)
-
-    def _loadKey(self, keyName, keyClass):
-        if not self.doesKeyExist(keyName, keyClass):
-            raise SecurityException("Key doesn't exist")
-
-        # Read the key data.
-        base64Content = None
-        keyPath = self._getTransformedName(keyName, keyClass)
-        with open(keyPath, 'r') as keyFile:
-            base64Content = keyFile.read()
-        bits  = base64.b64decode(base64Content)
-
-        return Blob(bits)
 
     def _saveKey(self, keyName, keyBits, keyClass):
         """
