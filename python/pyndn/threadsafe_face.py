4ad78c51fddf946d55491f6939632e08792c071d
Jeff Thompson <jefft0@remap.ucla.edu>
2015-07-02 14:08:51 -0700

    ThreadsafeFace: For the constructors ThreadsafeFace(loop) and ThreadsafeFace(loop, host, port) use AsyncTcpTransport or AsyncUnixTransport.

--- python/pyndn/threadsafe_face.py
+++ python/pyndn/threadsafe_face.py
@@ -22,22 +22,65 @@ This module defines the ThreadsafeFace class which extends Face to provide the
 main methods for NDN communication in a thread-safe manner.
 """
 
+from pyndn.util.common import Common
+from pyndn.transport.async_tcp_transport import AsyncTcpTransport
+from pyndn.transport.async_unix_transport import AsyncUnixTransport
 from pyndn.face import Face
 
 class ThreadsafeFace(Face):
     """
     Create a new ThreadsafeFace to use the asyncio loop to process events and
-    schedule communication calls. This constructor schedules the event loop
-    to process events, but you must start the loop on the thread in which
+    schedule communication calls. You must start the loop on the thread in which
     you want the library to call communication callbacks such as onData and
     onInterest.
     In Python <= 3.2, you must have the prerequisite Trollius library. See the
     INSTALL file for installation details. For usage, see the example
     test_get_async_threadsafe.py.
+    This constructor has the forms ThreadsafeFace(loop),
+    ThreadsafeFace(loop, transport, connectionInfo) or
+    ThreadsafeFace(loop, host, port). If the default Face(loop) constructor is
+    used, if the forwarder's Unix socket file exists then connect using
+    AsyncUnixTransport, otherwise connect to "localhost" on port 6363 using
+    AsyncTcpTransport. You do not need to call processEvents since the
+    asyncio loop does all processing. (Exception: If you pass a transport that
+    is not an async transport like AsyncTcpTransport, then you application
+    need to call processEvents.)
+
+    :param loop: The event loop, for example from asyncio.get_event_loop(). It
+      is the responsibility of the application to start and stop the loop.
+    :param Transport transport: An object of a subclass of Transport used for
+      communication. If you do not want to call processEvents, then the
+      transport should be an async transport like AsyncTcpTransport, in which
+      case the transport should use the same loop.
+    :param Transport.ConnectionInfo connectionInfo: An object of a subclass of
+      Transport.ConnectionInfo to be used to connect to the transport.
+    :param str host: In the Face(host, port) form of the constructor, host is
+      the host of the NDN hub to connect using TcpTransport.
+    :param int port: (optional) In the Face(host, port) form of the constructor,
+      port is the port of the NDN hub. If omitted. use 6363.
     """
-    def __init__(self, loop, arg1, arg2 = None):
-        super(ThreadsafeFace, self).__init__(arg1, arg2)
+    def __init__(self, loop, arg1 = None, arg2 = None):
         self._loop = loop
+
+        # Imitate the Face constructor, but use AsyncTcpTransport, etc.
+        if arg1 == None or Common.typeIsString(arg1):
+            filePath = ""
+            if arg1 == None and arg2 == None:
+                # Check if we can connect using UnixSocket.
+                filePath = self._getUnixSocketFilePathForLocalhost()
+
+            if filePath == "":
+                transport = AsyncTcpTransport(loop)
+                host = arg1 if arg1 != None else "localhost"
+                connectionInfo = AsyncTcpTransport.ConnectionInfo(
+                  host, arg2 if type(arg2) is int else 6363)
+            else:
+                transport = AsyncUnixTransport(loop)
+                connectionInfo = AsyncUnixTransport.ConnectionInfo(filePath)
+        else:
+            transport = arg1
+            connectionInfo = arg2
+        super(ThreadsafeFace, self).__init__(transport, connectionInfo)
 
         # Schedule the main processEvents service.
         self._loop.call_soon(self._processEventsCallback)
