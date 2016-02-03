537ccbfbaea38fc79f4ef33540a9e51ec63f6371
Jeff Thompson <jefft0@remap.ucla.edu>
2016-02-03 12:38:02 -0800

    setup.py: Change to version 2.2b1.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ elif sys.version_info[0] == 3:
 setup(
     name='PyNDN',
 
-    version='2.1b2',
+    version='2.2b1',
 
     description='An NDN client library with TLV wire format support in native Python',
 
