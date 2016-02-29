661bda6838271ddcb49470c33574be0801af270c
Jeff Thompson <jefft0@remap.ucla.edu>
2016-02-29 09:55:36 -0800

    unit_tests: In test_name_methods test_compare, added compare with subname tests from ndn-cxx. Refs #3449.

--- tests/unit_tests/test_name_methods.py
+++ tests/unit_tests/test_name_methods.py
@@ -154,6 +154,52 @@ class TestNameMethods(ut.TestCase):
         sortedURIs = [x.toUri() for x in sortedNames]
         self.assertEqual(sortedURIs, expectedOrder, 'Name comparison gave incorrect order')
 
+        # Tests from ndn-cxx name.t.cpp Compare.
+        self.assertEqual(Name("/A")  .compare(Name("/A")),    0)
+        self.assertEqual(Name("/A")  .compare(Name("/A")),    0)
+        self.assertTrue (Name("/A")  .compare(Name("/B"))   < 0)
+        self.assertTrue (Name("/B")  .compare(Name("/A"))   > 0)
+        self.assertTrue (Name("/A")  .compare(Name("/AA"))  < 0)
+        self.assertTrue (Name("/AA") .compare(Name("/A"))   > 0)
+        self.assertTrue (Name("/A")  .compare(Name("/A/C")) < 0)
+        self.assertTrue (Name("/A/C").compare(Name("/A"))   > 0)
+
+        self.assertEqual(Name("/Z/A/Y")  .compare(1, 1, Name("/A")),    0)
+        self.assertEqual(Name("/Z/A/Y")  .compare(1, 1, Name("/A")),    0)
+        self.assertTrue (Name("/Z/A/Y")  .compare(1, 1, Name("/B"))   < 0)
+        self.assertTrue (Name("/Z/B/Y")  .compare(1, 1, Name("/A"))   > 0)
+        self.assertTrue (Name("/Z/A/Y")  .compare(1, 1, Name("/AA"))  < 0)
+        self.assertTrue (Name("/Z/AA/Y") .compare(1, 1, Name("/A"))   > 0)
+        self.assertTrue (Name("/Z/A/Y")  .compare(1, 1, Name("/A/C")) < 0)
+        self.assertTrue (Name("/Z/A/C/Y").compare(1, 2, Name("/A"))   > 0)
+
+        self.assertEqual(Name("/Z/A")  .compare(1, 9, Name("/A")),    0)
+        self.assertEqual(Name("/Z/A")  .compare(1, 9, Name("/A")),    0)
+        self.assertTrue (Name("/Z/A")  .compare(1, 9, Name("/B"))   < 0)
+        self.assertTrue (Name("/Z/B")  .compare(1, 9, Name("/A"))   > 0)
+        self.assertTrue (Name("/Z/A")  .compare(1, 9, Name("/AA"))  < 0)
+        self.assertTrue (Name("/Z/AA") .compare(1, 9, Name("/A"))   > 0)
+        self.assertTrue (Name("/Z/A")  .compare(1, 9, Name("/A/C")) < 0)
+        self.assertTrue (Name("/Z/A/C").compare(1, 9, Name("/A"))   > 0)
+
+        self.assertEqual(Name("/Z/A/Y")  .compare(1, 1, Name("/X/A/W"),   1, 1),  0)
+        self.assertEqual(Name("/Z/A/Y")  .compare(1, 1, Name("/X/A/W"),   1, 1),  0)
+        self.assertTrue (Name("/Z/A/Y")  .compare(1, 1, Name("/X/B/W"),   1, 1) < 0)
+        self.assertTrue (Name("/Z/B/Y")  .compare(1, 1, Name("/X/A/W"),   1, 1) > 0)
+        self.assertTrue (Name("/Z/A/Y")  .compare(1, 1, Name("/X/AA/W"),  1, 1) < 0)
+        self.assertTrue (Name("/Z/AA/Y") .compare(1, 1, Name("/X/A/W"),   1, 1) > 0)
+        self.assertTrue (Name("/Z/A/Y")  .compare(1, 1, Name("/X/A/C/W"), 1, 2) < 0)
+        self.assertTrue (Name("/Z/A/C/Y").compare(1, 2, Name("/X/A/W"),   1, 1) > 0)
+
+        self.assertEqual(Name("/Z/A/Y")  .compare(1, 1, Name("/X/A"),   1),  0)
+        self.assertEqual(Name("/Z/A/Y")  .compare(1, 1, Name("/X/A"),   1),  0)
+        self.assertTrue (Name("/Z/A/Y")  .compare(1, 1, Name("/X/B"),   1) < 0)
+        self.assertTrue (Name("/Z/B/Y")  .compare(1, 1, Name("/X/A"),   1) > 0)
+        self.assertTrue (Name("/Z/A/Y")  .compare(1, 1, Name("/X/AA"),  1) < 0)
+        self.assertTrue (Name("/Z/AA/Y") .compare(1, 1, Name("/X/A"),   1) > 0)
+        self.assertTrue (Name("/Z/A/Y")  .compare(1, 1, Name("/X/A/C"), 1) < 0)
+        self.assertTrue (Name("/Z/A/C/Y").compare(1, 2, Name("/X/A"),   1) > 0)
+
     def test_match(self):
         name = Name("/edu/cmu/andrew/user/3498478")
         name2 = Name(name)
