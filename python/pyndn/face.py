8d34b8965a4a4b5c044d9b2526bb3297fc01aa22
Jeff Thompson <jefft0@remap.ucla.edu>
2016-05-04 13:16:05 -0700

    Face (internal): In _getExpressInterestArgs, process the overloaded arguments using the more general manner of registerPrefix.

--- python/pyndn/face.py
+++ python/pyndn/face.py
@@ -138,59 +138,44 @@ class Face(object):
           'interestCopy', 'onData', 'onTimeout' and 'wireFormat'.
         :rtype: dict
         """
-        # expressInterest(interest, onData)
-        # expressInterest(interest, onData, wireFormat)
-        # expressInterest(interest, onData, onTimeout)
-        # expressInterest(interest, onData, onTimeout, wireFormat)
         if type(interestOrName) is Interest:
             # Node.expressInterest requires a copy of the interest.
             interestCopy = Interest(interestOrName)
-            onData = arg2
-            if isinstance(arg3, WireFormat):
-                onTimeout = None
-                wireFormat = arg3
-            else:
-                onTimeout = arg3
-                wireFormat = arg4
         else:
             # The first argument is a name. Make the interest from the name and
             #   possible template.
-
-            # expressInterest(name, interestTemplate, onData)
-            # expressInterest(name, interestTemplate, onData, wireFormat)
-            # expressInterest(name, interestTemplate, onData, onTimeout)
-            # expressInterest(name, interestTemplate, onData, onTimeout, wireFormat)
             if type(arg2) is Interest:
                 template = arg2
                 # Copy the template.
                 interestCopy = Interest(template)
                 interestCopy.setName(interestOrName)
 
-                onData = arg3
-                if isinstance(arg4, WireFormat):
-                    onTimeout = None
-                    wireFormat = arg4
-                else:
-                    onTimeout = arg4
-                    wireFormat = arg5
-            # expressInterest(name, onData)
-            # expressInterest(name, onData, wireFormat)
-            # expressInterest(name, onData, onTimeout)
-            # expressInterest(name, onData, onTimeout, wireFormat)
+                # Shift the remaining args to be processed below.
+                arg2 = arg3
+                arg3 = arg4
+                arg4 = arg5
             else:
+                # No template.
                 interestCopy = Interest(interestOrName)
                 # Set a default interest lifetime.
                 interestCopy.setInterestLifetimeMilliseconds(4000.0)
-                onData = arg2
-                if isinstance(arg3, WireFormat):
-                    onTimeout = None
-                    wireFormat = arg3
-                else:
-                    onTimeout = arg3
-                    wireFormat = arg4
 
-        if wireFormat == None:
-            # Don't use a default argument since getDefaultWireFormat can change.
+        onData = arg2
+        # arg3, arg4 may be:
+        # OnTimeout,  WireFormat
+        # OnTimeout,  None
+        # WireFormat, None
+        # None,       None
+        if isinstance(arg3, collections.Callable):
+            onTimeout = arg3
+        else:
+            onTimeout = None
+
+        if isinstance(arg3, WireFormat):
+            wireFormat = arg3
+        elif isinstance(arg4, WireFormat):
+            wireFormat = arg4
+        else:
             wireFormat = WireFormat.getDefaultWireFormat()
 
         return { 'pendingInterestId': self._node.getNextEntryId(),
