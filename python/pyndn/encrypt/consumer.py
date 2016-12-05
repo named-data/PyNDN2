46987f24a93107636e4867571a42c4b70d01a789
Jeff Thompson <jefft0@remap.ucla.edu>
2016-12-05 13:45:54 -0800

    encrypt: Fix bug in Consumer._sendInterest: Need to pass link when calling itself.

--- python/pyndn/encrypt/consumer.py
+++ python/pyndn/encrypt/consumer.py
@@ -396,7 +396,7 @@ class Consumer(object):
 
         def onTimeout(interest):
             if nRetrials > 0:
-                self._sendInterest(interest, nRetrials - 1, onVerified, onError)
+                self._sendInterest(interest, nRetrials - 1, link, onVerified, onError)
             else:
                 onNetworkNack(interest, NetworkNack())
 
