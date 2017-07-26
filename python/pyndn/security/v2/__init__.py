8ccb7748c6fc18b3d2f603794f8562ac8d18bdb2
Jeff Thompson <jefft0@remap.ucla.edu>
2017-07-26 09:10:37 -0700

    security: Added CertficiateV2. Refs #4045.

---
+++ python/pyndn/security/v2/__init__.py
@@ -0,0 +1,29 @@
+# -*- Mode:python; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+#
+# Copyright (C) 2017 Regents of the University of California.
+# Author: Jeff Thompson <jefft0@remap.ucla.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# A copy of the GNU Lesser General Public License is in the file COPYING.
+
+from pyndn.security.v2 import certificate_v2
+__all__ = ['certificate_v2']
+
+import sys as _sys
+
+try:
+    from pyndn.security.v2.certificate_v2 import *
+except ImportError:
+    del _sys.modules[__name__]
+    raise
