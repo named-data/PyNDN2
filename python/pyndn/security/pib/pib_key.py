df9bf68132559fc4665b29a825ee88fe71c7795e
Jeff Thompson <jefft0@remap.ucla.edu>
2017-08-07 02:55:24 -0700

    security: Finish PibKey. Refs #4045.

--- python/pyndn/security/pib/pib_key.py
+++ python/pyndn/security/pib/pib_key.py
@@ -2,6 +2,7 @@
 #
 # Copyright (C) 2017 Regents of the University of California.
 # Author: Jeff Thompson <jefft0@remap.ucla.edu>
+# Author: From ndn-cxx security https://github.com/named-data/ndn-cxx/blob/master/src/security/pib/key.cpp
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -29,6 +30,14 @@ from pyndn.name import Name
 from pyndn.security.v2.certificate_v2 import CertificateV2
 
 class PibKey(object):
+    """
+    Create a PibKey which uses the impl backend implementation. This constructor 
+    should only be called by PibKeyContainer.
+
+    :param PibKeyImpl impl: An object of a subclass of  PibKeyImpl.
+    """
+    def __init__(self, impl):
+        self._impl = impl
 
     @staticmethod
     def constructKeyName(identityName, keyId):
@@ -73,3 +82,54 @@ class PibKey(object):
 
         # Trim everything after and including "KEY".
         return keyName.getPrefix(-CertificateV2.MIN_KEY_NAME_LENGTH)
+
+    def _addCertificate(self, certificate):
+        """
+        Add the certificate. If a certificate with the same name (without
+        implicit digest) already exists, then overwrite the certificate. If no
+        default certificate for the key has been set, then set the added
+        certificate as default for the key. This should only be called by
+        KeyChain.
+
+        :param CertificateV2 certificate: The certificate to add. This copies
+          the object.
+        :raises ValueError: If the name of the certificate does not match the
+          key name.
+        """
+        self._lockImpl().addCertificate(certificate)
+
+    def _removeCertificate(self, certificateName):
+        """
+        Remove the certificate with name certificateName. If the certificate
+        does not exist, do nothing. This should only be called by KeyChain.
+
+        :param Name certificateName: The name of the certificate.
+        :raises ValueError: If certificateName does not match the key name.
+        """
+        self._lockImpl().removeCertificate(certificateName)
+
+    def _setDefaultCertificate(self, certificateName):
+        """
+        Set the existing certificate with name certificateName as the default
+        certificate. This should only be called by KeyChain.
+
+        :param Name certificateName: The name of the certificate.
+        :return: The default certificate.
+        :rtype: CertificateV2
+        :raises ValueError: If certificateName does not match the key name.
+        :raises Pib.Error: If the certificate does not exist.
+        """
+        return self._lockImpl().setDefaultCertificate(certificateName)
+
+    def _lockImpl(self):
+        """
+        Check the validity of the _impl instance.
+
+        :return: The PibKeyImpl when the instance is valid.
+        :rtype: PibKeyImpl
+        :raises ValueError: If the backend implementation instance is invalid.
+        """
+        if self._impl == None:
+            raise ValueError("Invalid key instance")
+
+        return self._impl
