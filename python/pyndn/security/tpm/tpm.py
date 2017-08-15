a6619c0e73be2030e61afea1e2dc8a9ab83ed055
Jeff Thompson <jefft0@remap.ucla.edu>
2017-08-15 13:20:24 -0700

    security: Finish porting Tpm. Refs #4045.

--- python/pyndn/security/tpm/tpm.py
+++ python/pyndn/security/tpm/tpm.py
@@ -37,7 +37,27 @@ existence of private keys, get public keys for the private keys, sign, and
 decrypt the supplied buffers using managed private keys.
 """
 
+from pyndn.util.blob import Blob
+from pyndn.name import Name
+from pyndn.security.security_types import KeyType
+
 class Tpm(object):
+    def __init__(self, scheme, location, backEnd):
+        """
+        Create a new TPM instance with the specified location. This constructor
+        should only be called by KeyChain.
+
+        :param str scheme: The scheme for the TPM.
+        :param str location: The location for the TPM.
+        :param TpmBackEnd backEnd: The TPM back-end implementation.
+        """
+        # Name => TpmKeyHandle
+        self._keys = {}
+
+        self._scheme = scheme
+        self._location = location
+        self._backEnd = backEnd
+
     class Error(Exception):
         """
         Create a Tpm.Error which represents a semantic error in TPM processing.
@@ -46,3 +66,229 @@ class Tpm(object):
         """
         def __init__(self, message):
             super(Tpm.Error, self).__init__(message)
+
+    def getTpmLocator(self):
+        return self._scheme + ":" + self._location
+
+    def hasKey(self, keyName):
+        """
+        Check if the key with name keyName exists in the TPM.
+        
+        :param Name keyName: The name of the key.
+        :return: True if the key exists.
+        :rtype: bool
+        """
+        return self._backEnd.hasKey(keyName)
+
+    def getPublicKey(self, keyName):
+        """
+        Get the public portion of an asymmetric key pair with name keyName.
+
+        :param Name keyName: The name of the key.
+        :return: The encoded public key, or an isNull Blob if the key does not
+          exist.
+        :rtype: Blob
+        """
+        key = self._findKey(keyName)
+
+        if key == None:
+            return Blob()
+        else:
+            return key.derivePublicKey()
+
+    def sign(self, data, keyName, digestAlgorithm):
+        """
+        Compute a digital signature from the byte buffer using the key with name
+        keyName.
+
+        :param data: The input byte buffer.
+        :type data: an array which implements the buffer protocol
+        :param Name keyName: The name of the key.
+        :param digestAlgorithm: The digest algorithm for the signature.
+        :type digestAlgorithm: int from DigestAlgorithm
+        :return: The signature Blob, or an isNull Blob if the key does not
+          exist, or for an unrecognized digestAlgorithm.
+        :rtype: Blob
+        """
+        key = self._findKey(keyName)
+
+        if key == None:
+            return Blob()
+        else:
+            return key.sign(digestAlgorithm, data)
+
+    def decrypt(self, cipherText, keyName):
+        """
+        Return the plain text which is decrypted from cipherText using the key
+        with name keyName.
+
+        :param cipherText: The cipher text byte buffer.
+        :type cipherText: an array which implements the buffer protocol
+        :param Name keyName: The name of the key.
+        :return: The decrypted data, or an isNull Blob if the key does not exist.
+        :rtype: Blob
+        """
+        key = self._findKey(keyName)
+
+        if key == None:
+            return Blob()
+        else:
+            return key.decrypt(cipherText)
+
+    # TPM Management
+
+    def isTerminalMode(self):
+        """
+        Check if the TPM is in terminal mode.
+
+        :return: True if in terminal mode.
+        :rtype: bool
+        """
+        return self._backEnd.isTerminalMode()
+
+    def setTerminalMode(self, isTerminal):
+        """
+        Set the terminal mode of the TPM. In terminal mode, the TPM will not ask
+        for a password from the GUI.
+
+        :param bool isTerminal: True to enable terminal mode.
+        """
+        self._backEnd.setTerminalMode(isTerminal)
+
+    def isTpmLocked(self):
+        """
+        Check if the TPM is locked.
+
+        :return: True if the TPM is locked, otherwise False.
+        :rtype: bool
+        """
+        return self._backEnd.isTpmLocked()
+
+    def unlockTpm(self, password):
+        """
+        Unlock the TPM. If !isTerminalMode(), prompt for a password from the GUI.
+
+        :param password: The password to unlock TPM.
+        :type password: an array which implements the buffer protocol
+        :return: True if the TPM was unlocked.
+        :rtype: bool
+        """
+        return self._backEnd.unlockTpm(password)
+
+    def _getBackEnd(self):
+        """
+        Get the TpmBackEnd. This should only be called by KeyChain.
+
+        :rtype: TpmBackEnd
+        """
+        return self._backEnd
+
+    def _createKey(self, identityName, params):
+        """
+        Create a key for the identityName according to params. The created key
+        is named /<identityName>/[keyId]/KEY . This should only be called by
+        KeyChain.
+
+        :param Name identityName: The name if the identity.
+        :param KeyParams params: The KeyParams for creating the key.
+        :return: The name of the created key.
+        :rtype: Name
+        :raises Tpm.Error: If params is invalid or the key type is unsupported.
+        :raises TpmBackEnd.Error: If the key already exists or cannot be created.
+        """
+        if (params.getKeyType() == KeyType.RSA or
+            params.getKeyType() == KeyType.ECDSA):
+            keyHandle = self._backEnd.createKey(identityName, params)
+            keyName = keyHandle.getKeyName()
+            self._keys[keyName] = keyHandle
+            return keyName
+        else:
+            raise Tpm.Error("createKey: Unsupported key type")
+
+    def _deleteKey(self, keyName):
+        """
+        Delete the key with name keyName. If the key doesn't exist, do nothing.
+        Note: Continuing to use existing Key handles on a deleted key results in
+        undefined behavior. This should only be called by KeyChain.
+
+        :param Name keyName: The name of the key.
+        :raises TpmBackEnd.Error: If the deletion fails.
+        """
+        try:
+            self._keys.remove(keyName)
+        except KeyError:
+            # Do nothing if it doesn't exist.
+            pass
+
+        self._backEnd.deleteKey(keyName)
+
+    def _exportPrivateKey(keyName, password):
+        """
+        Get the encoded private key with name keyName in PKCS #8 format, possiby
+        encrypted. This should only be called by KeyChain.
+
+        :param Name keyName: The name of the key in the TPM.
+        :param password: The password for encrypting the private key. If the
+          password is supplied, use it to return a PKCS #8
+          EncryptedPrivateKeyInfo. If the password is None, return an
+          unencrypted PKCS #8 PrivateKeyInfo.
+        :type password: an array which implements the buffer protocol
+        :return: The private key encoded in PKCS #8 format, or an isNull Blob if
+          the key does not exist.
+        :rtype: Blob
+        :raises TpmBackEnd.Error: If the key does not exist or if the key cannot
+          be exported, e.g., insufficient privileges.
+        """
+        return self._backEnd.exportKey(keyName, password)
+
+    def _importKey(self, keyName, pkcs8, password):
+        """
+        Import an encoded private key with name keyName in PKCS #8 format,
+        possibly passwprd-encrypted. This should only be called by KeyChain.
+
+        :param Name keyName: The name of the key to use in the TPM.
+        :param pkcs8: The input byte buffer. If the password is supplied, this
+          is a PKCS #8 EncryptedPrivateKeyInfo. If the password is None, this is
+          an unencrypted PKCS #8 PrivateKeyInfo.
+        :type pkcs8: an array which implements the buffer protocol
+        :param password: The password for decrypting the private key. If the
+          password is supplied, use it to decrypt the PKCS #8
+          EncryptedPrivateKeyInfo. If the password is None, import an
+          unencrypted PKCS #8 PrivateKeyInfo.
+        :type password: an array which implements the buffer protocol
+        :return: True for success, False if importing fails.
+        :rtype: bool
+        """
+        try:
+            self._backEnd.importKey(keyName, pkcs8, password)
+        except:
+            return False
+
+        return True
+
+    def _findKey(self, keyName):
+        """
+        Get the TpmKeyHandle with name keyName, using _backEnd.getKeyHandle if
+        it is not already cached in _keys.
+
+        :param Name keyName: The name of the key, which is copied.
+        :return: The key handle in the _keys cache, or null if no key exists
+          with name keyName.
+        :rtype: TpmKeyHandle
+        """
+        try:
+            handle = self._keys[keyName]
+        except KeyError:
+            handle = None
+
+        if handle != None:
+            return handle
+
+        handle = self._backEnd.getKeyHandle(keyName)
+
+        if handle != None:
+            # Copy the Name.
+            self._keys[Name(keyName)] = handle
+            return handle
+
+        return None
