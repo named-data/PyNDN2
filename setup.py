576192b24e2448f0705cc10fcf3be66dba96fc80
Jeff Thompson <jefft0@remap.ucla.edu>
2018-04-17 15:28:10 -0700

    CHANGELOG: Tag release v2.8beta1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ elif sys.version_info[0] == 3:
 setup(
     name='PyNDN',
 
-    version='2.7b1',
+    version='2.8b1',
 
     description='An NDN client library with TLV wire format support in native Python',
 
