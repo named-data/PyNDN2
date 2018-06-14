f73f4a52a378b5dd863436b93393cc1fbc2c54e6
Jeff Thompson <jefft0@remap.ucla.edu>
2018-06-14 04:21:13 -0700

    util: In CommandInterestGenerator, use CommandInterestPreparer.

--- python/pyndn/util/command_interest_generator.py
+++ python/pyndn/util/command_interest_generator.py
@@ -21,27 +21,18 @@
 This module defines the CommandInterestGenerator class which keeps track of a
 timestamp and generates command interests according to the NFD Signed Command
 Interests protocol:
-http://redmine.named-data.net/projects/nfd/wiki/Command_Interests
+https://redmine.named-data.net/projects/ndn-cxx/wiki/CommandInterest
 """
 
-from random import SystemRandom
-from pyndn.name import Name
-from pyndn.util.blob import Blob
-from pyndn.util.common import Common
-from pyndn.encoding.tlv.tlv_encoder import TlvEncoder
+from pyndn.security.command_interest_preparer import CommandInterestPreparer
 from pyndn.encoding.wire_format import WireFormat
 
-# The Python documentation says "Use SystemRandom if you require a
-#   cryptographically secure pseudo-random number generator."
-# http://docs.python.org/2/library/random.html
-_systemRandom = SystemRandom()
-
-class CommandInterestGenerator(object):
+class CommandInterestGenerator(CommandInterestPreparer):
     """
     Create a new CommandInterestGenerator and initialize the timestamp to now.
     """
     def __init__(self):
-        self._lastTimestamp = round(Common.getNowMilliseconds())
+        super(CommandInterestGenerator, self).__init__()
 
     def generate(self, interest, keyChain, certificateName, wireFormat = None):
         """
@@ -65,28 +56,10 @@ class CommandInterestGenerator(object):
             # Don't use a default argument since getDefaultWireFormat can change.
             wireFormat = WireFormat.getDefaultWireFormat()
 
-        timestamp =  round(Common.getNowMilliseconds())
-        while timestamp <= self._lastTimestamp:
-          timestamp += 1.0
-
-        # The timestamp is encoded as a TLV nonNegativeInteger.
-        encoder = TlvEncoder(8)
-        encoder.writeNonNegativeInteger(int(timestamp))
-        interest.getName().append(Blob(encoder.getOutput(), False))
-
-        # The random value is a TLV nonNegativeInteger too, but we know it is 8
-        # bytes, so we don't need to call the nonNegativeInteger encoder.
-        randomBuffer = bytearray(8)
-        for i in range(len(randomBuffer)):
-            randomBuffer[i] = _systemRandom.randint(0, 0xff)
-        interest.getName().append(Blob(randomBuffer, False))
-
+        self.prepareCommandInterestName(interest, wireFormat)
         keyChain.sign(interest, certificateName, wireFormat)
 
         if (interest.getInterestLifetimeMilliseconds() == None or
             interest.getInterestLifetimeMilliseconds()< 0):
           # The caller has not set the interest lifetime, so set it here.
           interest.setInterestLifetimeMilliseconds(1000.0)
-
-        # We successfully signed the interest, so update the timestamp.
-        self._lastTimestamp = timestamp
