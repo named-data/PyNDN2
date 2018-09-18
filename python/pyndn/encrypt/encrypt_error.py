9e358dec63cbf11ae0a469132a91a93ab3f89079
Jeff Thompson <jefft0@remap.ucla.edu>
2018-09-18 12:00:29 -0700

    encrypt: In EncryptError, added V2 error codes. Refs #4655.

--- python/pyndn/encrypt/encrypt_error.py
+++ python/pyndn/encrypt/encrypt_error.py
@@ -26,11 +26,31 @@ hold more enums or functions.
 
 class EncryptError(object):
     class ErrorCode(object):
-        Timeout                     = 1
-        Validation                  = 2
-        UnsupportedEncryptionScheme = 32
-        InvalidEncryptedFormat      = 33
-        NoDecryptKey                = 34
-        EncryptionFailure           = 35
-        DataRetrievalFailure        = 36
-        General                     = 100
+        KekRetrievalFailure  = 1
+        KekRetrievalTimeout  = 2
+        KekInvalidName       = 3
+
+        KdkRetrievalFailure  = 11
+        KdkRetrievalTimeout  = 12
+        KdkInvalidName       = 13
+        KdkDecryptionFailure = 14
+
+        CkRetrievalFailure   = 21
+        CkRetrievalTimeout   = 22
+        CkInvalidName        = 23
+
+        MissingRequiredKeyLocator    = 101
+        TpmKeyNotFound               = 102
+        EncryptionFailure            = 103
+        DecryptionFailure            = 104
+        MissingRequiredInitialVector = 110
+
+        General                      = 200
+
+        # :deprecated: These codes are from the NAC library v1.
+        Timeout                     = 1001
+        Validation                  = 1002
+        UnsupportedEncryptionScheme = 1032
+        InvalidEncryptedFormat      = 1033
+        NoDecryptKey                = 1034
+        DataRetrievalFailure        = 1036
