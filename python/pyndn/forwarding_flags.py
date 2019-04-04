66dc4f165e3a3fd988338195c412b73b16b3c07a
Jeff Thompson <jefft0@remap.ucla.edu>
2019-04-04 12:38:47 -0700

    Deprecated ForwardingFlags and make it inherit from RegistrationOptions. Refs #4869.

--- python/pyndn/forwarding_flags.py
+++ python/pyndn/forwarding_flags.py
@@ -18,132 +18,22 @@
 # A copy of the GNU Lesser General Public License is in the file COPYING.
 
 """
-This module defines the ForwardingFlags class which holds the flags which
-specify how the forwarding daemon should forward an interest for a registered
-prefix.  We use a separate ForwardingFlags object to retain future compatibility
-if the daemon forwarding bits are changed, amended or deprecated.
+This module defines the ForwardingFlags class which is deprecated. Use
+RegistrationOptions.
 """
 
-class ForwardingFlags(object):
+from pyndn.registration_options import RegistrationOptions
+
+class ForwardingFlags(RegistrationOptions):
     """
     Create a new ForwardingFlags object, possibly copying values from another
     object.
 
-    :param ForwardingFlags value: (optional) If value is a ForwardingFlags, copy
-      its values.  If value is omitted, the type is the default with
-      "childInherit" True and other flags False.
+    :param RegistrationOptions value: (optional) If value is a
+      RegistrationOptions (or ForwardingFlags), copy its values. If value is
+      omitted, the type is the default with "childInherit" True and other flags
+      False.
+    :deprecated: Use RegistrationOptions.
     """
     def __init__(self, value = None):
-        if value == None:
-            self._childInherit = True
-            self._capture = False
-            self._origin = None
-        elif isinstance(value, ForwardingFlags):
-            # Copy its values.
-            self._childInherit = value._childInherit
-            self._capture = value._capture
-            self._origin = value._origin
-        else:
-            raise RuntimeError(
-              "Unrecognized type for ForwardingFlags constructor: " +
-              str(type(value)))
-
-    NfdForwardingFlags_CHILD_INHERIT = 1
-    NfdForwardingFlags_CAPTURE       = 2
-
-    def getNfdForwardingFlags(self):
-        """
-        Get an integer with the bits set according to the NFD forwarding flags
-        as used in the ControlParameters of the command interest.
-
-        :return: An integer with the bits set.
-        :rtype: int
-        """
-        result = 0
-
-        if self._childInherit:
-            result |= ForwardingFlags.NfdForwardingFlags_CHILD_INHERIT
-        if self._capture:
-            result |= ForwardingFlags.NfdForwardingFlags_CAPTURE
-
-        return result
-
-    def setNfdForwardingFlags(self, nfdForwardingFlags):
-        """
-        Set the flags according to the NFD forwarding flags as used in the
-        ControlParameters of the command interest.
-        This ignores the origin value.
-
-        :param int nfdForwardingFlags: An integer with the bits set.
-        :return: This ForwardingFlags so that you can chain calls to update values.
-        :rtype: ForwardingFlags
-        """
-        self._childInherit = True if (nfdForwardingFlags &
-                                      ForwardingFlags.NfdForwardingFlags_CHILD_INHERIT) else False
-        self._capture = True if (nfdForwardingFlags &
-                                 ForwardingFlags.NfdForwardingFlags_CAPTURE) else False
-        return self
-
-    def getChildInherit(self):
-        return self._childInherit
-
-    def getCapture(self):
-        return self._capture
-
-    def getOrigin(self):
-        """
-        Get the origin value.
-
-        :return: The origin value, or None if not specified.
-        :rtype: int
-        """
-        return self._origin
-
-    def setChildInherit(self, childInherit):
-        """
-        Set the value of the "childInherit" flag.
-
-        :param bool childInherit: True to set the "childInherit" flag, False to
-          clear it.
-        :return: This ForwardingFlags so that you can chain calls to update values.
-        :rtype: ForwardingFlags
-        """
-        self._childInherit = childInherit
-        return self
-
-    def setCapture(self, capture):
-        """
-        Set the value of the "capture" flag.
-
-        :param bool capture: True to set the "capture" flag, False to clear it.
-        :return: This ForwardingFlags so that you can chain calls to update values.
-        :rtype: ForwardingFlags
-        """
-        self._capture = capture
-        return self
-
-    def setOrigin(self, origin):
-        """
-        Set the origin value. This is used to set the origin value of the
-        ControlParameters for the register prefix command.
-
-        :param int origin: The new origin value, or None for not specified.
-        :return: This ForwardingFlags so that you can chain calls to update values.
-        :rtype: ForwardingFlags
-        """
-        self._origin = origin
-        return self
-
-    # Support property-based equivalence check
-    # TODO: Desired syntax?
-    def equals(self, other):
-        if (self._childInherit == other._childInherit
-        and self._capture == other._capture):
-            return True
-        else:
-            return False
-
-    # Create managed properties for read/write properties of the class for more pythonic syntax.
-    childInherit = property(getChildInherit, setChildInherit)
-    capture = property(getCapture, setCapture)
-    origin = property(getOrigin, setOrigin)
+        super(ForwardingFlags, self).__init__(value)
