f57778632b4eccfe1b090ac7d982884195ed1594
Jeff Thompson <jefft0@remap.ucla.edu>
2019-04-12 14:46:36 -0700

    unit_tests (minor): In test_invertible_bloom_lookup_table, fix the copyright year.

--- tests/unit_tests/test_invertible_bloom_lookup_table.py
+++ tests/unit_tests/test_invertible_bloom_lookup_table.py
@@ -1,6 +1,6 @@
 # -*- Mode:python; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
 #
-# Copyright (C) 2014-2019 Regents of the University of California.
+# Copyright (C) 2019 Regents of the University of California.
 # Author: Jeff Thompson <jefft0@remap.ucla.edu>
 # From ndn-cxx NamingConventions unit tests:
 # https://github.com/named-data/PSync/blob/master/tests/test-iblt.cpp
